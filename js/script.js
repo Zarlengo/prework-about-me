ea4463220c908e6801fd3c78ffbae77b24193c4b
Chris Zarlengo <christopher@zarlengo.net>
2020-05-20 03:50:51 -0700

    Removed PHP components, does not work as hosting on GitHub

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 window.onload = function() {
     window.scroll(0, 0);
 
+    var starDiv = document.getElementById("stars");
+    var x = 0;
+    var y = 0;
+    var r = 0;
+    var r_n = 0;
+    var t = 0;
+    var colorArray = ["white", "white", "red", "white", "white", "orange", "white", "white", "blue", "white", "white", "green", "white", "white"];
+    for (var star = 0; star < 1000; star++) {
+        var tag = document.createElement("s");
+        r = Math.random() * 220 + 4.9;
+        //if (r < 60) { r = r * 3;} else if (r < 120) { r = r * 2;}
+        t = Math.random() * 2 * Math.PI;
+        x = Math.floor((r * Math.cos(t) + 225) * 100) / 100;
+        y = Math.floor((r * Math.sin(t) + 225) * 100) / 100;
+        var randColor = Math.floor(Math.random() * (colorArray.length + 1));
+        starString = "top:" + y + "px;left:" + x + "px;background-color:" + colorArray[randColor] + ";";
+        tag.setAttribute('style', starString);
+        starDiv.appendChild(tag);
+    }
+
+
+
+
 
 };
 
